1ee1e315d9f8d9b9e32d38cff2981ae72d065c29
Mario Viapiano <mario@webionate.de>
2020-02-27 16:22:09 -0300

    Add behaviour to World

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rand::Rng;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
+#[repr(u8)]
 enum Cell {
-    Alive,
-    Dead,
+    Alive = 1,
+    Dead = 0,
 }
 
 #[derive(Debug)]
@@ -18,7 +19,7 @@ impl World {
         let mut cells: Vec<Cell> = Vec::new();
         let mut random_generator = rand::thread_rng();
 
-        for _n in 0..101 {
+        for _n in 0..9 {
             let random = random_generator.gen_range(0.0, 1.0);
             if random > 0.5 {
                 cells.push(Cell::Alive);
@@ -28,26 +29,47 @@ impl World {
         }
 
         World {
-            width: 10,
-            height: 10,
+            width: 3,
+            height: 3,
             cells,
         }
     }
 
     pub fn print(&self) {
-        for _row in 0..11 {
-            for column in 0..11 {
-                println!("{:?}", self.cells[column]);
+        for row in 0..3 {
+            for column in 0..3 {
+                print!("{:?} ", self.cell_at(row, column) as u8);
             }
             println!("\n");
         }
     }
+
+    fn cell_at(&self, row: u32, column: u32) -> Cell {
+        let index = (row * self.width + column) as usize;
+
+        self.cells[index]
+    }
+
+    fn cells_alive_around(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let cell = self.cell_at(neighbor_row, neighbor_col);
+                count += cell as u8;
+            }
+        }
+        count
+    }
 }
 
 fn main() {
-    let mut world: World = World::new();
-
-    world.cells.push(Cell::Dead);
-
+    let world: World = World::new();
     world.print();
+    println!("{}", world.cells_alive_around(2, 2));
 }
